a44c569656233af2e585e4bafbcf45af0e16185f
Uppelli Dayakar <dayakaruppelli@gmail.com>
2026-06-26 13:47:45 +0530

    Successfull configure the products app in the settings.py and connect the mysql database

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'product_browser',
+        'USER': 'root',
+        'PASSWORD': 'd@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
